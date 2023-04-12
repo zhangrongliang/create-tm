568aa0049dfa427d7d15086086e35ff7af25e179
zhangrongliang <zhangrongliang@aliyun.com>
2023-04-12 17:47:32 +0800

    upd 1.0.7

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const handleCreate = () => {
 const getCwd = () => process.cwd()
 const genFiles = options => {
 	//模版的目录
-	const templateSrc = path.resolve(__dirname, `./demo/${options.template}`)
+	const templateSrc = path.resolve(__dirname, `./templates/${options.template}`)
 	//项目指定生成目录，如果命令中没有有配置目录，则在当前命令运行的目录下生成以项目名称为名字的新目录
 	const destination = options.destination
 		? path.resolve(options.destination)
