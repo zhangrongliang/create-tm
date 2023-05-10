da7cb433fb66eaad0442c6771d49b3f3bb8af5c6
zhangrongliang <zhangrongliang@aliyun.com>
2023-05-10 14:09:44 +0800

    添加 vue-element-plus-admin 和 antd pro vue 模板

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ const handleCreate = () => {
 				message: 'Project Name:',
 				default: 'my-project',
 			},
-			/**
-			 * TODO 放置在 template 目录下的配合使用
-			 */
 			{
 				type: 'input',
 				name: 'description',
@@ -35,15 +32,21 @@ const handleCreate = () => {
 				type: 'list',
 				name: 'template',
 				message: 'Choose A Template',
-				choices: ['nuxt3-vue3-ant-design-vue', 'nuxt3-vue3-element-plus'],
+				choices: [
+					'nuxt3-vue3-ant-design-vue',
+					'nuxt3-vue3-element-plus',
+					'vue-element-plus-admin',
+					'ANTD PRO VUE',
+				],
 			},
 		])
 		.then(async answers => {
-			//根据回答以及选项，参数来生成项目文件
-			const processGenFiles = ora('开始创建项目……')
-			processGenFiles.start() // 进度条开始
+			// answers 用户交互数据
+			const process = ora('开始创建项目……')
+			// 进度条开始
+			process.start()
 			await genFiles(answers)
-			processGenFiles.succeed(`项目创建成功: ${answers.name}`)
+			process.succeed(`项目创建成功: ${answers.name}`)
 		})
 		.catch(error => {
 			console.error(error)
@@ -52,38 +55,28 @@ const handleCreate = () => {
 
 const clone = function (repo, desc) {
 	return new Promise((resolve, reject) => {
-		// https://www.npmjs.com/package/download-git-repo
-		const download = promisify(require('download-git-repo')) // download-git-repo: Download and extract a git repository (GitHub, GitLab, Bitbucket)
-		const ora = require('ora')
+		const download = promisify(require('download-git-repo'))
 		const process = ora(`下载模板中...${repo}`)
 		;(async () => {
-			process.start() // 进度条开始
+			// 进度条开始
+			process.start()
 			await download(repo, desc.name, { clone: true })
-			//  download-git-repo导出的download方法，第一个参数repo是仓库地址，格式有三种：
-			// GitHub - github:owner/name or simply owner/name
-			// GitLab - gitlab:owner/name
-			// Bitbucket - bitbucket:owner/name
 			process.succeed(`模板下载成功: ${desc.template}`)
 			resolve()
 		})()
 	})
 }
 
-//获得命令运行时的路径
-const getCwd = () => process.cwd()
-
-// 下载模板
-const genFiles = options => {
+const copy = function (options) {
 	return new Promise((resolve, reject) => {
-		/**
-		 * TODO 放置在 template 目录下的配合使用
-		 */
+		// 获得命令运行时的路径
+		const getCwd = () => process.cwd()
 		//模版的目录
 		const templateSrc = path.resolve(
 			__dirname,
 			`./templates/${options.template}`
 		)
-		//项目指定生成目录，如果命令中没有有配置目录，则在当前命令运行的目录下生成以项目名称为名字的新目录
+		// 项目生成目录
 		const destination = options.destination
 			? path.resolve(options.destination)
 			: path.resolve(getCwd(), options.name)
@@ -118,18 +111,31 @@ const genFiles = options => {
 				}
 				resolve()
 			})
-		// Gitlab-通用模板(vue3 nuxt3，完全纯静态、SSR 服务器渲染、PWA 渐进式)
-		// if (
-		// 	options.template ===
-		// 	'Gitlab-通用模板(vue3 nuxt3，完全纯静态、SSR 服务器渲染、PWA 渐进式)'
-		// ) {
-		// 	clone(
-		// 		'direct:http://172.16.0.20/tm_java/tm_web/other/template/vue3-templates.git',
-		// 		options
-		// 	).then(() => {
-		// 		resolve()
-		// 	})
-		// }
+	})
+}
+
+// 下载模板
+const genFiles = options => {
+	return new Promise((resolve, reject) => {
+		if (options.template === 'vue-element-plus-admin') {
+			clone(
+				'direct:https://gitee.com/kailong110120130/vue-element-plus-admin.git',
+				options
+			).then(() => {
+				resolve()
+			})
+		} else if (options.template === 'ANTD PRO VUE') {
+			clone(
+				'direct:https://github.com/vueComponent/ant-design-vue-pro.git',
+				options
+			).then(() => {
+				resolve()
+			})
+		} else {
+			copy(options).then(() => {
+				resolve()
+			})
+		}
 	})
 }
 
